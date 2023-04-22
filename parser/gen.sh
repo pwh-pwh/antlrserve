419b6c9318ad191b1ad0ba4dcb5a2d7111184db3
pwh-pwh <plmoknpwh@163.com>
2023-04-22 21:11:48 +0800

    update gen.sh

--- parser/gen.sh
+++ parser/gen.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 #echo '开始生成antlr文件'
-antlr4 gm/MinicGram.g4
-javac gm/MinicGram*.java
 alias grun='java -Xmx500M -cp "/usr/local/lib/antlr-4.12.0-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig'
 alias antlr4='java -Xmx500M -cp "/usr/local/lib/antlr-4.12.0-complete.jar:$CLASSPATH" org.antlr.v4.Tool'
+antlr4 gm/MinicGram.g4
+javac gm/MinicGram*.java
+
 cd gm
 grun MinicGram program -ps ../img/out.ps ../example/input.txt
